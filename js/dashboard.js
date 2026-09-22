0bac4ed51f6890444f1164ede43405428e681130
RevanPM <revanx23@gmail.com>
2026-09-23 00:32:01 +0200

    Update database version from 1 to 2

--- js/dashboard.js
+++ js/dashboard.js
@@ -107,7 +107,7 @@ async function loadEquipRefData() {
   }
 }
 loadEquipRefData();
-const DB_VERSION = "1"; 
+const DB_VERSION = "2"; 
 async function loadDatabase() {
   const SQL = await initSqlJs({
     locateFile: (file) =>
