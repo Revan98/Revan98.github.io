580101dd723383ab57796199c20d4c7ce69b89ca
RevanPM <revanx23@gmail.com>
2026-09-23 22:13:30 +0200

    Update database version from 4 to 5

--- js/dashboard.js
+++ js/dashboard.js
@@ -107,7 +107,7 @@ async function loadEquipRefData() {
   }
 }
 loadEquipRefData();
-const DB_VERSION = "4"; 
+const DB_VERSION = "5"; 
 async function loadDatabase() {
   const SQL = await initSqlJs({
     locateFile: (file) =>
